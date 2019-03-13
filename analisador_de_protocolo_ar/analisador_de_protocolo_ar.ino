49802b8ba98893a126e834c5803af83b30368834
josenyldo <josenildo.simao@gmail.com>
2019-03-13 17:30:54 -0300

    Foi colocado para printar uma string durante 2 segundos após detectar uma informação útil do controle

--- analisador_de_protocolo_ar/analisador_de_protocolo_ar.ino
+++ analisador_de_protocolo_ar/analisador_de_protocolo_ar.ino
@@ -1,12 +1,33 @@
 int pino = 7;
-
+int leituraPino = 0;
+int palvraBinaria[200];
+int tempo_total = 0;
+int meutempo = 0;
 void setup()
 {
 
   pinMode(pino, INPUT);
   Serial.begin(9600);
+  
 }
 void loop()
 {
-  Serial.println(digitalRead(pino));
+  
+  leituraPino = digitalRead(pino);
+  //Serial.println(leituraPino);
+  
+  if(leituraPino == 0)
+  {
+    meutempo = millis();
+    while(tempo_total < 2000)
+    {   
+      
+      tempo_total = millis() - meutempo;
+      Serial.print(tempo_total);
+      Serial.print(" ");
+      Serial.println("Nildao");
+    }
+    
+  }
 }
+
