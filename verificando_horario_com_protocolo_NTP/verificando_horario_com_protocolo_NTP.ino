42069a8db4511bb9da7ce32a15d0319a2cc5ea8e
Josenyldo <josenildo.simao@gmail.com>
2021-11-12 09:10:33 -0300

    Monstrando Conteudo no Display OLED.

--- verificando_horario_com_protocolo_NTP/verificando_horario_com_protocolo_NTP.ino
+++ verificando_horario_com_protocolo_NTP/verificando_horario_com_protocolo_NTP.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <Wire.h>
+#include "SSD1306Wire.h"
+SSD1306Wire  display(0x3c, D2, D3);
+void setup()
+{
+  Serial.begin(115200);
+  display.init();
+  display.flipScreenVertically();
+}
+void texto()
+{
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(63, 10, "Desculpe");
+  display.drawString(63, 26, "Brito");
+  display.display();
+}
+void horario()
+{
+  //Apaga o display
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  //Seleciona a fonte
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(63, 10, "Hora");
+  display.display();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  texto();
+  delay(3000);
+  horario();
+  delay(3000);
 }
